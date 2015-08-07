ab0147a38cd9d4772cb7deaed505dba856925262
Mark T. Holder <mtholder@gmail.com>
2015-08-07 11:11:45 -0500

    removed cruft and got closer on diff calc - still need to pass in monomorphic state there

--- doc/demo-neutral-pomo.py
+++ doc/demo-neutral-pomo.py
@@ -81,7 +81,7 @@ print '\n'.join(['{} {}'.format(i, s) for i, s in enumerate(S.STATES)])
 params = {}
 params['NUC_FREQ'] = [0.1, 0.2, 0.3, 0.4]
 params['GTR_RATE'] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
-params['PROB_POLY'] = 0.01 # Equil. prob. being polymorphic
+params['PROB_POLY'] = 0.1 # Equil. prob. being polymorphic
 
 def neut_pomo_qmat(params):
     nuc_freqs = params['NUC_FREQ']
@@ -142,8 +142,8 @@ def neut_pomo_qmat(params):
                         q_el = K * poly_transform*VIRTUAL_POP_SIZE*(VIRTUAL_POP_SIZE - 1)
                 elif j_diallele == i_diallele:
                     i_count_in_i = S.diallele_count(i, i_diallele, i)
-                    j_count_in_i = S.diallele_count(i, i_diallele, j)
-                    diff = i_count_in_i - j_count_in_i
+                    i_count_in_j = S.diallele_count(j, i_diallele, i)
+                    diff = i_count_in_i - i_count_in_j
                     if (diff == 1) or (diff == -1):
                         # drift. eqn 14
                         q_el = i_count_in_i * (VIRTUAL_POP_SIZE - i_count_in_i)/float(VIRTUAL_POP_SIZE)
@@ -170,84 +170,3 @@ for i, row in enumerate(p):
   rs = '   '.join(['{:10.4f}'.format(el) for el in row])
   label = 'P[{}][*] ='.format(i)
   print '{:10} {}'.format(label, rs)
-
-sys.exit(0)
-
-def lmh_pomo_qmat(params):
-  two_plus_psi = 2.0 + mid_freq_param
-  freq_for_bins_given_poly = [1/two_plus_psi, mid_freq_param/two_plus_psi, 1/two_plus_psi]
-
-  raw_mat = [[0.0]*NUM_POMO_STATES for i in range(NUM_POMO_STATES)]
-  mono_over_poly = (1.0 - prob_poly)/prob_poly
-  K = 0.0
-  for i in range(4):
-    for j in range(i + 1, 4):
-      pi_prod = nuc_freqs[i]*nuc_freqs[j]
-      k_el = pi_prod*sym_mu_mat[i][j]
-      K += k_el
-  loss_rate = K*mono_over_poly*two_plus_psi
-  
-  # first the gain of/loss of element
-  for mono in [S.A, S.C, S.G, S.T]:
-    for other, bins_with_other in S.POLY_LOOKUP[mono].items():
-      pi_other = nuc_freqs[other]
-      to_other_ind = bins_with_other[0] # first element is the high freq of mono, low of other
-      q_new_mut = pi_other*sym_mu_mat[mono][other]
-      raw_mat[mono][to_other_ind] = q_new_mut
-      raw_mat[to_other_ind][mono] = loss_rate
-  # Set the transitions that correspond to changes in allele freq among polymorphic states (not mutation or fixation)
-  from_mid_coefficient = prob_poly*drift_rate/(K*two_plus_psi)
-  to_mid_coefficient = from_mid_coefficient*mid_freq_param
-  for poly_ind in range(4, NUM_POMO_STATES):
-    if poly_ind in S.IND_MID:
-      I, J = S.IND_MID[poly_ind]
-      alleles_sp_factor = nuc_freqs[I]*nuc_freqs[J]*sym_mu_mat[I][J]
-      q = from_mid_coefficient*alleles_sp_factor
-      raw_mat[poly_ind][poly_ind + 1] = q
-      raw_mat[poly_ind][poly_ind - 1] = q
-    else:
-      if poly_ind in S.IND_LOW:
-        I, J = S.IND_LOW[poly_ind]
-        neighbor = poly_ind + 1
-      else:
-        assert poly_ind in S.IND_HIGH
-        I, J = S.IND_HIGH[poly_ind]
-        neighbor = poly_ind - 1
-      alleles_sp_factor = nuc_freqs[I]*nuc_freqs[J]*sym_mu_mat[I][J]
-      q = to_mid_coefficient * alleles_sp_factor
-      raw_mat[poly_ind][neighbor] = q
-  # Set diagonal
-  for row_ind in range(NUM_POMO_STATES):
-    row_sum = sum(raw_mat[row_ind])
-    assert raw_mat[row_ind][row_ind] == 0.0
-    raw_mat[row_ind][row_ind] = -1*row_sum
-
-  # Set scaler so that exit from monomorphic states = 1
-  mono_exit_rate = 0.0
-  for i in range(4):
-    i_exit_rate = 0.0
-    for j, r in enumerate(raw_mat[i]):
-      if r > 0.0:
-        if j in S.IND_LOW:
-          I, J = S.IND_LOW[j]
-        else:
-          assert j in S.IND_HIGH
-          I, J = S.IND_HIGH[j]
-        assert (I == i) or (J == i)
-        alleles_sp_factor = nuc_freqs[I]*nuc_freqs[J]*sym_mu_mat[I][J]
-        to_mid_rate = to_mid_coefficient*alleles_sp_factor
-        prob_next_move_to_mid = to_mid_rate/(to_mid_rate + loss_rate)
-        # once an allele makes to 50/50 freq, its' probability of substitution is 50%
-        eventual_subst_prob = 0.5*prob_next_move_to_mid
-        i_to_j_subst_rate = eventual_subst_prob*r
-        i_exit_rate += i_to_j_subst_rate
-    mono_exit_rate += nuc_freqs[i]*i_exit_rate
-  mono_exit_rate *= (1.0 - prob_poly)
-  scaler = 1.0/mono_exit_rate
-  for row in raw_mat:
-    for i in range(NUM_POMO_STATES):
-      row[i] *= scaler
-  print 'scaler =', scaler
-  return raw_mat
-
-
